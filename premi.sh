f46178657796b0c6d332281e6618f7fbc2f61fda
ahmadsoleh12 <149921554+ahmadsoleh12@users.noreply.github.com>
2023-12-11 10:23:39 +0700

    Update premi.sh

--- premi.sh
+++ premi.sh
@@ -80,11 +80,11 @@ red='\e[1;31m'
 green='\e[0;32m'
 NC='\e[0m'
 #IZIN SCRIPT
-ipsaya=$(curl -sS ipv4.icanhazip.com)
+MYIP=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m"
 clear
 #IZIN SCRIPT
-MYIP=$(curl -sS ipv4.icanhazip.com)
+ipsaya=$(curl -sS ipv4.icanhazip.com)
 echo -e "\e[32mloading...\e[0m" 
 clear
 # Version sc
